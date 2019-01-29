986e18a79856d1664022b868794d680d2675a088
Manu Patil <mpatil99@gmail.com>
2019-01-28 23:32:45 -0500

    Adding hello.c optimization exercise

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int x = 5;
+    printf("%i",x);
+    int y = x + 1;
+    printf("%i",y);
     return 0;
 }
+/*
+1subq	$16, %rsp
+movl	$5, -4(%rbp)
+
+These were the lines added
+the number following the $ is the value stored at an address?
+2  The assembly file becomes shorter
+
+3 Again, the assembly file shrinks
+
+4 Once, more the number of lines is reduced
+
+5 I would assume that that the optimize function optimize the number of lines
+produced in the assembly file
+*/
