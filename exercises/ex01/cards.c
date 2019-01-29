9f07ffe4a1b84d4a922b65793be6c076bc42d91c
Manu Patil <mpatil99@gmail.com>
2019-01-28 22:50:49 -0500

    Updating to three method version

--- exercises/ex01/cards.c
+++ exercises/ex01/cards.c
@@ -6,14 +6,51 @@ Head First C Cards
 
 #include <stdio.h>
 #include <stdlib.h>
+void usr_Input(char *card_name);
+int adjust_Count(int val);
+void count_Cards();
 int main()
 {
+  count_Cards(3);
+  return 0;
+}
+
+/* Prompts user for input and puts it in the given buffer
+  User input is truncated to the first two characters.
+
+  card_name: buffer where result is stored
+*/
+void usr_Input(char *card_name){
+  puts("Enter the card_name: ");
+  scanf("%2s", card_name);
+}
+
+/* Takes a value of current card and adjusts count appropriately.
+If card is high, count is reduced.
+If card is low, count is increased.
+
+val: value of cards
+*/
+int adjust_Count(int val){
+  if((val>2) && (val<7)){
+    return 1;
+  } else if (val == 10){
+    return -1;
+  } else{
+    return 0;
+  }
+}
+/* Counts Cards to increase odds of winning at the popular game
+of blackjack
+Allows player to decide what the best time to place large and small bets
+count: The initial value of the value telling an user whether to place a bet or not
+*/
+void count_Cards(int count){
   char card_name[3];
-  int count = 3;
+  int val = 0;
   do {
-    puts("Enter the card_name: ");
-    scanf("%2s", card_name);
-    int val = 0;
+    usr_Input(card_name);
+    val = 0;
     switch(card_name[0]){
       case 'K':
       case 'Q':
@@ -21,7 +58,7 @@ int main()
         val = 10;
         break;
       case 'A':
-        val = 10;
+        val = 11;
         break;
       case 'X':
         continue;
@@ -32,13 +69,8 @@ int main()
           continue;
         }
     }
-    if((val>2) && (val<7)){
-      count++;
-    } else if (val == 10){
-      count--;
-    }
+    count +=adjust_Count(val);
     printf("Current count: %i\n", count);
 
   } while (card_name[0] != 'X');
-  return 0;
 }
