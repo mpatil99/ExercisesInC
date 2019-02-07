41d7a184e72bf3fe355f23c70e34a1d393fa19e7
Manu Patil <mpatil99@gmail.com>
2019-02-07 18:53:26 -0500

    Adding aspace and stack

--- exercises/ex02/aspace.c
+++ exercises/ex02/aspace.c
@@ -9,18 +9,42 @@ License: GNU GPLv3
 #include <stdlib.h>
 
 int var1;
+void aprinter(int var);
 
 int main ()
 {
     int var2 = 5;
     void *p = malloc(128);
+    void *t = malloc(128);
     char *s = "Hello, World";
 
     printf ("Address of main is %p\n", main);
+    printf ("Address of main is %p\n", aprinter);
     printf ("Address of var1 is %p\n", &var1);
+    aprinter(var2);
     printf ("Address of var2 is %p\n", &var2);
     printf ("p points to %p\n", p);
+    printf ("t points to %p\n", t);
     printf ("s points to %p\n", s);
+
+    int *n = malloc(32);
+    int *k = malloc(32);
+
+    printf ("n points to %p\n", n);
+    printf ("k points to %p\n", k);
+
+
 
     return 0;
 }
+
+void aprinter(int var){
+  printf ("Address of var is %p\n", &var);
+}
+/*
+3. Image in Notes
+4. The Heap does grow upwords
+5. I am rather confused about what the question is asking. But the area in the
+stack did grow down when this function was called
+6. The space between them is 33 bytes
+*/
--- exercises/ex02/stack.c
+++ exercises/ex02/stack.c
@@ -14,7 +14,7 @@ int *foo() {
     int i;
     int array[SIZE];
 
-    //printf("%p\n", array);
+    printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
         array[i] = 42;
@@ -26,7 +26,7 @@ void bar() {
     int i;
     int array[SIZE];
 
-    //printf("%p\n", array);
+    printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
         array[i] = i;
@@ -45,3 +45,24 @@ int main()
 
     return 0;
 }
+/*
+1. The code appears to be be filling and array of size 5 with the element 42 in
+each position.
+The main method than prints it
+
+2. I get a warning that foo returns the address of a local variable. Because an
+array is created directly in stack space, the address is in the current stack
+frame. This could cause issues when another stack frame is plopped on top.
+
+3. I get a segmentation fault. This probably occurs because the bar() method
+creates another stack frame where the array is previously stored.
+
+4. The address of the array created in bar() is the same as the address of the
+ array created in the foo() method.
+
+5. Weird things happen when you return a pointer to stack allocated memory.
+First, when a new stack frame is created you will run into issues because that
+area is being overwritten, which C will happily do.
+Second, when a pointer to a new address is created it will return a pointer to
+the same address that held the older array
+*/
