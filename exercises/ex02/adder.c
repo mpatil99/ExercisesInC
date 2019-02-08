5235d00870bd42d6e5dbf74dced51bf23e48895d
Manu Patil <mpatil99@gmail.com>
2019-02-08 11:08:22 -0500

    Adding adder.c

---
+++ exercises/ex02/adder.c
@@ -0,0 +1,52 @@
+/* SoftSys 2019 Hw
+
+Manu Patil
+License: GNU GPLv3
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int get_usr_input(char* in,int* position, int length){
+  if(fgets(in, 5, stdin)!= NULL) {
+    return atoi(in);
+  }
+  else {
+    printf("Thanks\n");
+    *position = length;
+    return 0;
+  }
+}
+
+int calc_sum(int* vals, int length){
+  int j,sum = 0;
+  for (j = 0; j < length; j++) {
+    sum += *(vals+j);
+    printf("Element[%d] = %d\n", j, *(vals+j));
+  }
+  return sum;
+}
+
+int main(){
+  int input[10];
+  char inputstr[4];
+  int i, length = 0;
+  int sum = 0;
+  // i = 0;
+  puts("Enter number and press \"Ctrl D\" to escape: ");
+  while(i < 10){
+    length = i;
+    input[i] = get_usr_input(inputstr,&i,10);
+    ++i;
+  }
+
+  sum = calc_sum(input, length);
+
+  printf("Your final sum is: %d\n",sum);
+
+
+
+  return 0;
+
+}
